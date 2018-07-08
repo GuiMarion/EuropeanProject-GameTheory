1cb1ab12110a3845b74fa027b6430d9e1c24530b
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-07-08 16:05:07 +0200

    Script

--- Script.py
+++ Script.py
@@ -19,10 +19,12 @@ def writeCountries(DataBase, file):
 	for i in range(len(sorted_by_value)):
 		file.write(str(i+1) + " : " + str(sorted_by_value[i][0]) + " -> " + str(sorted_by_value[i][1]) + "\n")
 
-def compute(gpa):
+def compute(gpa, nb):
+
+	print("Start:", str(gpa))
 
 	file = open("Results/"+str(gpa)+".txt",'w')
-	DataBase = getDatabase(gpa, 2)
+	DataBase = getDatabase(gpa, nb)
 	file.write("				Country List (nb of projects) \n \n")
 	writeCountries(DataBase, file)
 	file.write("\n \n")
@@ -35,25 +37,28 @@ def compute(gpa):
 
 	file.close()
 
+	print("End:", str(gpa))
+
 
 def main(nb):
 
 	GPAs = [[], ['H2020-EU.3'], ['H2020-EU.1.3.1'], ['H2020-EU.3.1'], ['H2020-EU.3.2'],  ['H2020-EU.3.3'],  ['H2020-EU.3.4'], ['H2020-EU.3.5'], ['H2020-EU.3.6'], ['H2020-EU.3.7']]
 
-	for i in range(len(GPAs)):
-		t1 = Thread(target=compute, args=(GPAs[i],))
+	for i in range(0, len(GPAs), 3):
+		t1 = Thread(target=compute, args=(GPAs[i],nb,))
 		t1.start()
 
 		if i +1 < len(GPAs):
-			t2 = Thread(target=compute, args=(GPAs[i+1],))
+			t2 = Thread(target=compute, args=(GPAs[i+1],nb,))
 			t2.start()
 
 		if i +2 < len(GPAs):
-			t3 = Thread(target=compute, args=(GPAs[i+2],))
+			t3 = Thread(target=compute, args=(GPAs[i+2],nb,))
 			t3.start()
 
-		sleep(60)
-
+		t1.join()
+		t2.join()
+		t3.join()
 main(2)
 
 
